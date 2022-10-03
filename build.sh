bf61239641d862db61ab909db9b8c214948a0618
1Conan <me@1conan.com>
2022-10-03 12:11:50 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+COMMON_FLAGS=(
+  "archive"
+  "-configuration release"
+  "-project GRDBCustom.xcodeproj"
+  "-scheme GRDBCustom"
+)
+
+SDKS=(
+  "iphoneos"
+  "iphonesimulator"
+  "macosx"
+)
+
+rm -rf GRDB.xcframework.zip
+
+FRAMEWORKS=()
+for SDK in "${SDKS[@]}"; do
+  echo "Running xcodebuild -sdk ${SDK} ${COMMON_FLAGS[@]} -archivePath GRDB-${SDK}.xcarchive"
+
+  xcodebuild \
+    -sdk ${SDK} \
+    ${COMMON_FLAGS[@]} \
+    -archivePath GRDB-${SDK}.xcarchive \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    SKIP_INSTALL=NO
+
+  FRAMEWORKS+=(
+    "-framework" "GRDB-${SDK}.xcarchive/Products/Library/Frameworks/GRDB.framework"
+    "-debug-symbols" "$(pwd)/GRDB-${SDK}.xcarchive/dSYMs/GRDB.framework.dSYM"
+  )
+done
+
+xcodebuild -create-xcframework -output GRDB.xcframework ${FRAMEWORKS[@]}
+
+zip -r GRDB.xcframework.zip GRDB.xcframework
+rm -rf ${ARCHIVES[@]} GRDB.xcframework
+
+echo
+echo
+
+sha256sum GRDB.xcframework.zip
